514c14ad0201f18a278dcc255e20e81d7f0004a0
asus-PC\asus <bggtank0102@gmail.com>
2019-04-27 17:36:45 +0800

    MergeKSortedLists

---
+++ MergeKSortedLists/MergeKSortedLists/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeKSortedLists
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListNode[] lists = new ListNode[3];
+            lists[0] = new ListNode(1);
+            lists[0].next = new ListNode(4);
+            lists[0].next.next = new ListNode(5);
+            lists[1] = new ListNode(1);
+            lists[1].next = new ListNode(3);
+            lists[1].next.next = new ListNode(4);
+            lists[2] = new ListNode(2);
+            lists[2].next = new ListNode(6);
+            ListNode head = MergeKLists(lists);
+        }
+        public static ListNode MergeKLists(ListNode[] lists)
+        {
+            ListNode head = new ListNode(0);
+            ListNode currency = head;
+            int min_Index = -1;
+            int min_Val = 0;
+            while (true)
+            {
+                min_Index = -1;
+                for (int i = 0; i < lists.Length; i++)
+                {
+                    if (min_Index == -1 && lists[i] != null)
+                    {
+                        min_Val = lists[i].val;
+                        min_Index = i;
+                        continue;
+                    }
+                    if (lists[i] != null && min_Val > lists[i].val)
+                    {
+                        min_Val = lists[i].val;
+                        min_Index = i;
+                    }
+                }
+                if (min_Index == -1)
+                {
+                    break;
+                }
+                currency.next = new ListNode(lists[min_Index].val);
+                currency = currency.next;
+                lists[min_Index] = lists[min_Index].next;
+            }
+            return head.next;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
